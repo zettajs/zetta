cbc48b158e43a848aa2b81c04fbcc5851e95e60c
Adam Magaluk <AdamMagaluK@gmail.com>
2014-07-10 16:22:08 -0400

    Added file for data_stream tests

---
+++ test/test_data_stream.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var PubSub = require('../lib/pubsub_service');
+var DataStream = require('../lib/data_stream');
+
+describe('DataStream', function() {
+  it('exposes sub', function() {
+  });
+});
