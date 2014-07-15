f54065f9d0490cd039f5b80ee25128c54746b39f
Kevin Swiber <kswiber@gmail.com>
2014-07-15 13:55:59 -0400

    Adding peer registry tests.

---
+++ test/test_peer_registry.js
@@ -0,0 +1,100 @@
+var assert = require('assert');
+var path = require('path');
+var level = require('level');
+var leveldown = require('leveldown');
+var PeerRegistry = require('../lib/peer_registry');
+
+var dbPath = path.join(__dirname, './.peers');
+
+describe('Peer Registry', function() {
+  var db;
+
+  beforeEach(function(done) {
+    db = level(dbPath);
+    done();
+  });
+
+  afterEach(function(done) {
+    if (db) {
+      db.close(function() {
+        leveldown.destroy(dbPath, done);
+      });
+    }
+  });
+
+  it('should find multiple peers', function(done) {
+    var reg = new PeerRegistry(db);
+    reg.save({ id: 0 }, function() {
+      reg.save({ id: 1 }, function() {
+        var query = { match: function() { return true; } };
+        reg.find(query, function(err, results) {
+          assert.equal(results.length, 2);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should get peers by id', function(done) {
+    var reg = new PeerRegistry(db);
+    reg.save({ id: 012345 }, function() {
+      reg.get(012345, function(err, peer) {
+        assert(peer);
+        done();
+      });
+    });
+  });
+
+  it('should delete peers', function(done) {
+    var reg = new PeerRegistry(db);
+    var peer = { id: 0123456 };
+    reg.save(peer, function() {
+      reg.remove(peer, function(err, peer) {
+        assert.ifError(err);
+        done();
+      });
+    });
+  });
+
+  it('should close', function(done) {
+    var reg = new PeerRegistry(db);
+    reg.close(function(err) {
+      assert.ifError(err);
+      done();
+    });
+  });
+
+  describe('#add', function() {
+    it('should save new peers', function(done) {
+      var reg = new PeerRegistry(db);
+      var peer = { id: 012345 };
+      
+      reg.add(peer, function(err, result) {
+        assert.ok(result);
+        done();
+      });
+    });
+
+    it('should generate an ID for new peers', function(done) {
+      var reg = new PeerRegistry(db);
+      var peer = { id: 012345 };
+      
+      reg.add(peer, function(err, result) {
+        assert.ok(result.id);
+        done();
+      });
+    });
+
+    it('should update existing peers', function(done) {
+      var reg = new PeerRegistry(db);
+      var peer = { id: 012345 };
+      
+      reg.save(peer, function() {
+        reg.add(peer, function(err, result) {
+          assert.equal(result.id, peer.id);
+          done();
+        });
+      });
+    });
+  });
+});
