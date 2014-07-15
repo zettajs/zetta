8c0aa37af44e90e3cd65dc287bf5d7ef8e48f4a4
Adam Magaluk <AdamMagaluK@gmail.com>
2014-07-15 11:15:05 -0400

    Added object stream test

--- sample/binary_stream_read.js
+++ sample/binary_stream_read.js
@@ -1,3 +1,5 @@
+// to test, first start IHeardThat example node sample/IHeardDat/index.js
+
 var fs = require('fs');
 var websocket = require('websocket-stream')
 var ws = websocket('ws://localhost:3000/servers/d7fe2c66-db23-4513-a5f5-a2a890d1af36/devices/0b335a88-35f8-49d9-b737-81f6456bf938/somevar');
