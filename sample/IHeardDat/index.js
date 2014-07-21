dfa970b16e85c5340dfc375ba4c5c93affef41a3
Adam Magaluk <AdamMagaluK@gmail.com>
2014-07-21 13:44:09 -0400

    calling setImediate when making initial peer exchange request to fix connection bug with hundreds of incomming connectionsy

--- sample/IHeardDat/index.js
+++ sample/IHeardDat/index.js
@@ -2,6 +2,11 @@ var zetta = require('../../zetta_runtime.js');
 var Arduino = require('./devices/arduino');
 var IHeardThat = require('./apps');
 
+var Logger = require('../../lib/Logger');
+function Logger () {
+  Logger.call(this);
+}
+
 var app = zetta();
 
 app.id = 'd7fe2c66-db23-4513-a5f5-a2a890d1af36';
