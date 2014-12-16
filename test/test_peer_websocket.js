61408ab45226d1c0d1fd84ab4e0e37a1f752b0b9
Adam Magaluk <AdamMagaluK@gmail.com>
2014-12-16 10:42:55 -0500

    Added tests for peer web_socket https/wss connection

---
+++ test/test_peer_websocket.js
@@ -0,0 +1,48 @@
+var assert = require('assert');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var WebSocketServer = require('ws').Server;
+var Websocket = require('../lib/web_socket');
+
+describe('Peer Client Websocket', function() {
+  
+  it('it should connect to ws:// server', function(done) {
+    var server = http.createServer();
+    var wss = new WebSocketServer({ server: server });
+    server.listen(0, function(err) {
+      if (err) {
+        return done(err);
+      }
+      var address = 'ws://' + server.address().address + ':' + server.address().port;
+      var ws = new Websocket(address);
+      ws.on('error', done);
+      ws.on('open', function() {
+        done();
+      });
+    });
+  });
+
+  it('it should connect to wss:// server', function(done) {
+    var opts = {
+      key: fs.readFileSync('./test/fixture/server.key'),
+      cert: fs.readFileSync('./test/fixture/server.crt')
+    };
+
+    var server = https.createServer(opts);
+    var wss = new WebSocketServer({ server: server });
+    server.listen(0, function(err) {
+      if (err) {
+        return done(err);
+      }
+
+      var address = 'wss://' + server.address().address + ':' + server.address().port;
+      var ws = new Websocket(address, { rejectUnauthorized: false});
+      ws.on('error', done);
+      ws.on('open', function() {
+        done();
+      });
+    });
+  });
+   
+});
