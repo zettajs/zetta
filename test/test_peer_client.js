70312be97f6a3f8da614f96dc5548e0a2e75366e
Adam Magaluk <AdamMagaluk@gmail.com>
2016-06-14 17:01:04 -0400

    Emit and Publish Peer Connection Errors (#317)
    * Emit & Publish Peer Connection Errors
    
    A zetta instance's `pubsub` can be used to react to peer client
    disconnect events. Currently, the _peer_client_ module will emit a
    "closed" event for both a socket error *and* socket close event. This
    means the zetta handle of 'error' events will never be called.
    
    This change adds `emit('error', err)` to the _peer_client_ module, and
    updates _zetta_ to publish the '_peer/disconnect' event with the
    `error` object as an additional property of the published data.
    
    `publish('_peer/disconnect'), {peer:peerClient, error: error});`
    
    This allows code that subscribes to the peer disconnect events to
    react specifically to errors.
    
    * Publish Error
    
    Updated _zetta_ module to publish '_peer/error' instead of disconnect
    on a peer client 'error' event. This should not cause an issues with
    existing code because the 'error' event was previously never emitted.
    
    * Update _peer_client_ to only emit the "error" event.
    
    https://github.com/zettajs/zetta/pull/312#issuecomment-220089700
    
    * Updated PR per @AdamMagaluk; changes to match _event_socket_ in:
    
    https://github.com/zettajs/zetta/commit/6206dc5ab199e0b9b039e1deac3074ee2f88db99
    
    * Added tests to ensure PeerClients emits the proper close event
    
    * Remove use of _peer/error and use only _peer/disconnect

--- test/test_peer_client.js
+++ test/test_peer_client.js
@@ -1,8 +1,19 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 var PeerClient = require('../lib/peer_client');
 var assert = require('assert');
 
 
-var MockServer = { _name: '1234', httpServer: { spdyServer: {}}, log: {}};
+var MockServer = { _name: '1234', httpServer: { spdyServer: {}}, log: {
+  emit: function() {}
+}};
+var MockSocket = function() {
+  EventEmitter.call(this);
+  this.setAddress = function() {};
+  this.start = function() {};
+};
+util.inherits(MockSocket, EventEmitter);
+
 var urlEndingWithSlash = 'http://cloud.zettajs.io/';
 var urlEndingWithNoSlash = 'http://cloud.zettajs.io';
 
@@ -16,6 +27,41 @@ describe('Peer Client', function() {
     it('should calculate the proper url without a trailing slash', function() {
       var client = new PeerClient(urlEndingWithNoSlash, MockServer); 
       assert.equal(client.url, 'ws://cloud.zettajs.io/peers/1234');
-    });
+    });    
   });
+
+  it('should emit error when underlying ws does', function(done) {
+    var client = new PeerClient(urlEndingWithNoSlash, MockServer);
+    client.ws = new MockSocket();
+    client._createSocket();
+    client.once('error', function(err) {
+      assert.equal(err.message, 'some message');
+      done();
+    });
+
+    client.once('closed', function() {
+      done(new Error('Should not have emitted closed'));
+    });
+
+    setTimeout(function() {
+      client.ws.emit('error', new Error('some message'));
+    }, 2);
+  })
+
+  it('should emit closed when underlying ws does', function(done) {
+    var client = new PeerClient(urlEndingWithNoSlash, MockServer);
+    client.ws = new MockSocket();
+    client._createSocket();
+    client.once('error', function(err) {
+      done(new Error('Should not have emitted error'));
+    });
+
+    client.once('closed', function() {
+      done();
+    });
+
+    setTimeout(function() {
+      client.ws.emit('close');
+    }, 2);
+  })
 });
